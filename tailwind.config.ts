bd395b20d00d39cb2337dfb36bb15cc45479a903
jnayeh <mouradslim44@gmail.com>
2023-06-17 01:21:22 +0100

    adds dark mode to app

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,34 +1,38 @@
 import { type Config } from "tailwindcss";
 
 export default {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  darkMode: "class",
   theme: {
-    darkMode: 'class',
     extend: {
       colors: {
-        primary: '#De3429',
-        secondary: '#151B40',
+        primary: "#De3429",
+        secondary: "#151B40",
         gray: {
-          200: '#F6F9FF',
+          200: "#F6F9FF",
         },
       },
       height: {
-        '10v': '10vh',
-        '20v': '20vh',
-        '30v': '30vh',
-        '40v': '40vh',
-        '50v': '50vh',
-        '60v': '60vh',
-        '70v': '70vh',
-        '80v': '80vh',
-        '90v': '90vh',
-        '100v': '100vh',
+        "10v": "10vh",
+        "20v": "20vh",
+        "30v": "30vh",
+        "40v": "40vh",
+        "50v": "50vh",
+        "60v": "60vh",
+        "70v": "70vh",
+        "80v": "80vh",
+        "90v": "90vh",
+        "100v": "100vh",
       },
       screens: {
-        xs: '300px',
-        '2xs': '440px',
+        xs: "300px",
+        "2xs": "440px",
 
-        '2xl': '1536px',
+        "2xl": "1536px",
         // => @media (min-width: 1536px) { ... }
       },
     },
