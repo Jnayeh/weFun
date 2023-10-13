2ced6633b38a3def8841a8e94a859ed5266c6c8e
jnayeh <mouradslim44@gmail.com>
2023-10-13 07:28:50 +0100

    adds category page

---
+++ src/pages/categories/[id].tsx
@@ -0,0 +1,27 @@
+import Head from "next/head";
+import { cn } from "@/lib/utils";
+import { NextPageWithLayout } from "~/pages/_app";
+
+const CategoriesPage: NextPageWithLayout = () => {
+  return (
+    <>
+      <Head>
+        <title>Categories - Find your new experiences</title>
+        <meta
+          name="description"
+          content="Details about the category"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main
+        className={cn(
+          " mx-auto flex min-h-[300px] max-w-[97%] flex-col items-center gap-2 py-4"
+        )}
+      >
+        heyy
+      </main>
+    </>
+  );
+};
+CategoriesPage.getLayout = (page: React.ReactNode) => <>{page}</>;
+export default CategoriesPage;
