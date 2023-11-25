225cd627a37f8cbc9785d7814ee64257b5b6577c
jnayeh <mouradslim44@gmail.com>
2023-11-25 13:47:29 +0100

    changes how the masonry loading component looks

--- src/components/MasonryGrid.tsx
+++ src/components/MasonryGrid.tsx
@@ -71,11 +71,11 @@ export function MasonryGridSkeleton(props: MasonryGridProps) {
         {[0, 1, 3, 4, 5, 6, 7, 8, 9].map((i) => (
                 <div
                   key={i}
-                  className={cn(`relative h-60 ${i%3 == 1 ? "h-96" : i%3 == 2 ? "h-48" : "" }  flex flex-col justify-end hover:translate-y-1 dark:bg-slate-600 animate-pulse rounded-2xl bg-slate-400`)}
+                  className={cn(`relative h-60 ${i%3 == 1 ? "h-96" : i%3 == 2 ? "h-48" : "" }  flex flex-col justify-end  dark:bg-slate-600 animate-pulse rounded-2xl bg-slate-400`)}
                 >
                   <Skeleton
                     id="img"
-                    className="h-5 w-[80%] m-2 animate-pulse rounded-2xl bg-slate-400"
+                    className="h-5 w-[80%] m-2 animate-pulse rounded-2xl dark:bg-slate-400 bg-slate-200"
                   />
                 </div>
               ))}
