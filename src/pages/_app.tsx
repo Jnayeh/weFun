5606dbd3df81e882e99504a647b052a60ae2e6e2
jnayeh <mouradslim44@gmail.com>
2024-01-13 20:09:15 +0100

    changes the app file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,10 @@ import { AppProps, type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import "~/styles/navbar.styles.css";
-import "~/styles/footer.styles.css";
-import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeProvider } from "~/utils/theme-provider";
 import { NextPage } from "next";
 import { NextIntlProvider } from "next-intl";
 import { ClerkProvider } from "@clerk/nextjs";
-import { AnimatePresence } from "framer-motion";
 
 export type NextPageWithLayout<P = {}> = NextPage<P> & {
   getLayout?: (page: React.ReactNode) => React.ReactNode;
@@ -22,12 +20,7 @@ const MyApp: AppType = ({ Component, pageProps, router }: WithLayoutProps) => {
     <ClerkProvider {...pageProps}>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
         <NextIntlProvider messages={pageProps.messages}>
-          <AnimatePresence
-            mode="wait"
-            initial={false}
-          >
             {getLayout(<Component {...pageProps} key={router.asPath} />)}
-          </AnimatePresence>
         </NextIntlProvider>
       </ThemeProvider>
     </ClerkProvider>
