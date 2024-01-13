e7fc5df39eaf998ed5bd73b1bb27c2859e116e6f
jnayeh <mouradslim44@gmail.com>
2024-01-14 00:48:51 +0100

    fixes drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,12 @@
 import type { Config } from "drizzle-kit";
  
+import { env } from "~/env.mjs";
+
 export default {
   schema: "./src/db/schema.ts",
   out: "./drizzle",
   driver: "pg",
   dbCredentials: {
-    connectionString: "postgresql://all_we_do:act_pass@localhost:5432/barmejha"
+    connectionString: env.DATABASE_URL
   }
 } satisfies Config;
