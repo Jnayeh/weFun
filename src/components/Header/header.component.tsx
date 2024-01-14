78e90c6f8c2cd9635be46ec6d4294b3197bf18fe
jnayeh <mouradslim44@gmail.com>
2024-01-14 16:12:07 +0100

    changes hero image

--- src/components/Header/header.component.tsx
+++ src/components/Header/header.component.tsx
@@ -24,7 +24,7 @@ const Header = () => {
       </div>
       <div className="relative flex w-full items-center justify-center md:mt-0 md:w-1/2 ">
         <Image
-          src="/Frame_hero_pic.svg"
+          src="/Frame_hero_pic.png"
           alt="man doing activity"
           className=" z-0 h-[370px] w-auto md:h-auto md:w-full"
           loading="eager"
