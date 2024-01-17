8730d484ddbd73e4ae91bf9fd0196a6a930bcd24
jnayeh <mouradslim44@gmail.com>
2024-01-17 01:27:38 +0100

    removes pages router

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
+const withNextIntl = require('next-intl/plugin')();
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.mjs");
-
+import("./src/env.mjs");
 /** @type {import("next").NextConfig} */
-const config = {
+module.exports = withNextIntl({
   reactStrictMode: false,
   images: {
     remotePatterns: [
@@ -28,11 +28,10 @@ const config = {
    * out.
    *
    * @see https://github.com/vercel/next.js/issues/41980
-   */
+   
   i18n: {
     locales: ["en","ar-tn","ar","fr"],
     defaultLocale: "ar-tn",
     localeDetection: false
-  },
-};
-export default config;
+  },*/
+});
--- src/pages/api/migrate.ts
+++
@@ -1,8 +0,0 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import { migrate } from "drizzle-orm/postgres-js/migrator";
-import postgres from "postgres";
-import { env } from "~/env.mjs";
-
-const migrationClient = postgres(env.DATABASE_URL, { max: 1 });
-await migrate(drizzle(migrationClient), { migrationsFolder: "drizzle" });
-await migrationClient.end();
