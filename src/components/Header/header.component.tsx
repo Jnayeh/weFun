e8baa57fda20742cd1d4f99e515d73c85b17ae84
jnayeh <mouradslim44@gmail.com>
2024-02-01 09:28:11 +0100

    changes hero pic

--- src/components/Header/header.component.tsx
+++ src/components/Header/header.component.tsx
@@ -4,13 +4,14 @@ import { Link } from "~/navigation";
 const Header = () => {
   return (
     <header className="mx-auto  flex max-w-[1600px] flex-col-reverse items-center justify-center pb-4 md:flex-row md:px-12 md:pt-4 ">
-      <div className="flex w-4/5 flex-col  items-center sm:pt-0 md:w-1/2 md:items-start">
-        <h1 className=" flex flex-col justify-start text-center text-2xl font-extrabold leading-none dark:text-white md:text-start md:text-[2rem] lg:text-[3.5rem] xl:text-7xl">
-          Make your daily experiences{" "}
-          <span className="text-primary "> memorable </span>
+      <div className="flex w-4/5 flex-col  items-center sm:pt-0 md:w-1/2 md:items-start ">
+        <h1 className=" flex flex-col justify-start text-center text-4xl font-extrabold dark:text-white md:text-start md:text-5xl lg:text-6xl xl:text-7xl font-marhey">
+          Barmej Kharja 
+          <span className="text-primary ">Maa الشلة</span>
         </h1>
-        <p className="py-4 text-center text-base font-semibold dark:text-white md:text-start lg:text-2xl xl:text-5xl">
-          Practice your hobbies with people who have the same interests as you.
+        <p className="py-4 text-center text-base font-semibold dark:text-white md:text-2xl xl:text-3xl font-marhey uppercase">
+        <span>و أصنع جوك مع صحابك Plani</span> <br/>
+        new connections ولا
         </p>
         <Link
           href="explore"
@@ -19,12 +20,12 @@ const Header = () => {
           sm:top-[290px] sm:scale-100 md:h-12 
           md:w-60 md:text-lg lg:text-lg xl:top-[400px]  xl:text-2xl"
         >
-          Find out more
+          Barmejha
         </Link>
       </div>
       <div className="relative flex w-full items-center justify-center md:mt-0 md:w-1/2 ">
         <Image
-          src="/Frame_hero_pic.png"
+          src="/Frame_hero_friends.png"
           alt="man doing activity"
           className=" z-0 h-[370px] w-auto md:h-auto md:w-full"
           loading="eager"
