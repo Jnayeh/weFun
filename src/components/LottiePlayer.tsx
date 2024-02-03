a8065333795b2b61e5d894e0519acb3b9c306f03
Mourad Slim Jnayeh <mourad.jnayeh@keyrus.com>
2024-02-03 08:00:48 +0100

    adds lottie player

---
+++ src/components/LottiePlayer.tsx
@@ -0,0 +1,6 @@
+"use client";
+import { DotLottiePlayer, Props } from "@dotlottie/react-player";
+import "@dotlottie/react-player/dist/index.css";
+export function LottiePlayer(props: Props) {
+  return <DotLottiePlayer {...props}></DotLottiePlayer>;
+}
