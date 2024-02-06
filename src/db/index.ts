9061fb4896e2131eb85ccd51a1f0294a684fbdbb
Jnayeh <mouradslim44@gmail.com>
2024-02-06 22:54:37 +0100

    makes db connection async

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,10 @@
-import mysql from "mysql2";
+import mysql from "mysql2/promise";
 import { drizzle } from "drizzle-orm/mysql2";
 import { env } from "~/env.mjs";
 import { registerService } from "~/utils/helpers/server";
 
-const client = registerService("DB", () => {
+let client = await registerService("DB", () => {
   return mysql.createConnection(env.DATABASE_URL);
 });
+
 export const db = drizzle(client, { logger: true });
