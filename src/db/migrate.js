eae7879520057f52a07904cbb4f26d66495f5185
Jnayeh <mouradslim44@gmail.com>
2024-02-08 10:34:58 +0100

    makes migration DB connection async and destroys connection after if connection was established

--- src/db/migrate.js
+++ src/db/migrate.js
@@ -1,23 +1,29 @@
 const { migrate } = require("drizzle-orm/mysql2/migrator");
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 const { drizzle } = require("drizzle-orm/mysql2");
 var { once } = require("lodash");
 
 /**
- * @type {import("drizzle-orm/mysql2").MySql2Client | import("mysql2").Connection | import("mysql2").Pool | undefined}
+ * @type {import("mysql2/promise").Connection | undefined}
  */
-const connect = mysql.createConnection(process.env.DATABASE_URL ?? "");
+let connect;
 
 const migrateDB = once(async () => {
   try {
+    if (connect === undefined) {
+      connect = await mysql.createConnection(process.env.DATABASE_URL ?? "");
+    }
     const db = drizzle(connect, { logger: true });
     await migrate(db, { migrationsFolder: "./drizzle" });
+    await connect.end();
     connect.destroy();
     console.log("Migration complete");
   } catch (err) {
     console.error(err);
-    connect.destroy();
     console.warn("Migration failed");
+  } finally {
+    connect?.destroy();
+    console.info("Connection destroyed");
   }
 });
 module.exports = {
