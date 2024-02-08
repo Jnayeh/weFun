573960d687bcbbb4483730b9d9c07c22d091d68c
Jnayeh <mouradslim44@gmail.com>
2024-02-08 11:38:05 +0100

    tries to fix the migration

--- src/db/migrate.js
+++ src/db/migrate.js
@@ -1,6 +1,7 @@
 const { migrate } = require("drizzle-orm/mysql2/migrator");
 const mysql = require("mysql2/promise");
 const { drizzle } = require("drizzle-orm/mysql2");
+// @ts-ignore
 var { once } = require("lodash");
 
 /**
@@ -8,24 +9,44 @@ var { once } = require("lodash");
  */
 let connect;
 
-const migrateDB = once(async () => {
-  try {
-    if (connect === undefined) {
-      connect = await mysql.createConnection(process.env.DATABASE_URL ?? "");
+/**
+ * @param {string} name
+ * @param {() => Promise<void>} initFn
+ * @returns {Promise<void>}
+ */
+function registerService(name, initFn) {
+  if (process.env.NODE_ENV === "development") {
+    if (!(name in global)) {
+      console.log("reusing client");
+
+      // @ts-ignore
+      global[name] = initFn();
     }
-    const db = drizzle(connect, { logger: true });
-    await migrate(db, { migrationsFolder: "./drizzle" });
-    await connect.end();
-    connect.destroy();
-    console.log("Migration complete");
-  } catch (err) {
-    console.error(err);
-    console.warn("Migration failed");
-  } finally {
-    connect?.destroy();
-    console.info("Connection destroyed");
+    // @ts-ignore
+    return global[name];
   }
-});
+  return initFn();
+}
+const migrateDB = () => {
+  registerService("migrateDB", async () => {
+    try {
+      if (connect === undefined) {
+        connect = await mysql.createConnection(process.env.DATABASE_URL ?? "");
+      }
+      const db = drizzle(connect, { logger: true });
+      await migrate(db, { migrationsFolder: "./drizzle" });
+      await connect.end();
+      connect.destroy();
+      console.log("Migration complete");
+    } catch (err) {
+      console.error(err);
+      console.warn("Migration failed");
+    } finally {
+      connect?.destroy();
+      console.info("Connection destroyed");
+    }
+  });
+};
 module.exports = {
   migrateDB,
 };
