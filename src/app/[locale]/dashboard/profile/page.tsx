738b3c4af02cad2b836d91d12ad7957caf5571ea
Jnayeh <mouradslim44@gmail.com>
2024-02-16 22:39:50 +0100

    changes dashboard pages content

--- src/app/[locale]/dashboard/profile/page.tsx
+++ src/app/[locale]/dashboard/profile/page.tsx
@@ -1,35 +1,10 @@
 import Link from "next/link";
 import React from "react";
 
-import { cachableGetActivities } from "~/app/[locale]/(visitor)/activities/page";
-
 const ActivitiesPage: React.FC = async () => {
-  const activities = await cachableGetActivities({ name: " " });
-
   return (
     <main className=" mx-auto flex w-[80%] flex-col items-center pt-10">
-      <h1 className=" text-pretty p-5 text-3xl font-bold">Activities</h1>
-      <Link href="activities/activity-form" className="self-end p-2 px-4 font-bold bg-red-600 rounded-lg">
-        Add activity
-      </Link>
-      <table className=" p-2">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Description</th>
-          </tr>
-        </thead>
-        <tbody>
-          {activities.map((activity) => (
-            <tr key={activity.id} className=" p-2">
-              <td className=" p-2">{activity.id}</td>
-              <td className=" p-2">{activity.label}</td>
-              <td className=" p-2">{activity.description}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <h1 className=" text-pretty p-5 text-3xl font-bold">Profile</h1>
     </main>
   );
 };
