f3f43b0e2e9f9407688deb79fe79cf9caa14baa0
Jnayeh <mouradslim44@gmail.com>
2024-02-17 11:35:58 +0100

    changes review carousel styling

--- src/components/ui/carousels/review-carousel.tsx
+++ src/components/ui/carousels/review-carousel.tsx
@@ -39,18 +39,20 @@ export function ReviewCarousel(
       <CarouselContent className="pt-7">
         {reviews.map((_, index) => (
           <CarouselItem key={index} className="md:basis-1/3 lg:basis-1/5">
-            <Card className="relative aspect-square flex flex-col gap-2 justify-between items-start ">
-              <Avatar className=" absolute left-4 -top-6 h-12 w-12 ">
+            <Card className="relative flex min-h-20 flex-col items-start justify-between gap-2 ">
+              <Avatar className=" absolute -top-6 left-4 h-12 w-12 ">
                 <AvatarImage src={_.user.image} alt="@shadcn" />
                 <AvatarFallback>{getInitials(_.user.name)}</AvatarFallback>
               </Avatar>
               <CardContent className=" p-4 pt-10 text-start">
-                <span className=" text-base font-semibold">{_.description}</span>
-              </CardContent>
-              
-              <span className=" line-clamp-1 text-blue-700 drop-shadow-md py-2 px-4">
-                  {_.user.name}
+                <span className=" text-base font-semibold">
+                  {_.description}
                 </span>
+              </CardContent>
+
+              <span className=" line-clamp-1 px-4 py-2 text-blue-700 drop-shadow-md dark:text-slate-400">
+                {_.user.name}
+              </span>
             </Card>
           </CarouselItem>
         ))}
