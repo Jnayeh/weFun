134c1684ddccfcef2de3653e5da5eff6b71a0021
Jnayeh <mouradslim44@gmail.com>
2024-02-24 00:58:15 +0100

    changes hero section

--- src/components/Header/header.component.tsx
+++ src/components/Header/header.component.tsx
@@ -1,42 +1,98 @@
 import Image from "next/image";
 import { Link } from "~/navigation";
+import HeaderArrow from "../SvgStore/header-arrow";
+import { cn } from "~/utils/helpers/server";
+import { dynamicBlurDataUrl } from "~/server/actions";
+import { Suspense } from "react";
 
 const Header = () => {
   return (
-    <header className="sticky top-0 z-0 mx-auto flex max-w-[1600px] flex-col-reverse items-center justify-center pb-4 md:flex-row md:px-12 md:pt-4 ">
-      <div className="flex w-4/5 flex-col  items-center sm:pt-0 md:w-1/2 md:items-start ">
-        <h1 className=" flex flex-col justify-start text-center text-4xl font-extrabold dark:text-white md:text-start md:text-5xl lg:text-6xl xl:text-7xl font-marhey">
-          Barmej Kharja 
-          <span className="text-primary ">Maa الشلة</span>
+    <header
+      className={cn(
+        " bg-[#E95B47] relative mx-auto flex h-[calc(100dvh-72px)] md:h-dvh md:h-screen max-h-[1080px] min-h-[600px] flex-col items-center justify-center pb-20 md:px-12 md:pb-4 md:pt-4"
+      )}
+    >
+      <Image
+        priority
+        src="/new_hero.jpg"
+        alt="man doing activity"
+        className="absolute "
+        loading="eager"
+        fill
+        fetchPriority="high"
+      />
+      <Suspense>
+        <FloatingCards />
+      </Suspense>
+      <div className="z-0 flex w-full flex-col items-center justify-center text-white shadow-black drop-shadow">
+        <h1 className=" flex flex-col justify-center text-center text-4xl font-extrabold dark:text-white sm:text-5xl md:text-6xl lg:text-7xl">
+          <span className="flex justify-center">
+            <span className="block h-fit w-fit bg-black px-1 pb-3">Barmej</span>{" "}
+            Kharja
+          </span>
+          <span className="leading-snug ">m3a Shella</span>
         </h1>
-        <p className="py-4 text-center text-base font-semibold dark:text-white md:text-2xl xl:text-3xl font-marhey uppercase">
-        <span>و أصنع جوك مع صحابك Plani</span> <br/>
-        new connections ولا
+        <p className="py-4 text-center font-marhey text-base font-semibold uppercase dark:text-white xs:text-xl md:text-2xl xl:text-3xl">
+          <span>و أصنع جوك مع صحابك Plani</span> <br />
+          new connections ولا
         </p>
-        <Link
-          href="explore"
-          className="flex h-[71px] w-[90%]
-         max-w-sm items-center justify-center rounded-[32px] bg-primary font-bold text-white hover:bg-red-700 2xs:text-2xl
-          sm:top-[290px] sm:scale-100 md:h-12 
-          md:w-60 md:text-lg lg:text-lg xl:top-[400px]  xl:text-2xl"
-        >
-          Barmejha
-        </Link>
-      </div>
-      <div className="relative flex w-full items-center justify-center md:mt-0 md:w-1/2 ">
-        <Image
-          src="/Frame_hero_friends.png"
-          alt="man doing activity"
-          className=" z-0 h-[370px] w-auto md:h-auto md:w-full"
-          loading="eager"
-          priority
-          fetchPriority="high"
-          width={600}
-          height={600}
-        />
+        <div className="flex w-[90%] max-w-sm justify-center ">
+          <HeaderArrow className="-scale-x-100" />
+          <Link
+            href="explore"
+            className="mt-5 flex h-fit py-2 px-5 items-center justify-center
+            rounded-full bg-slate-50 font-bold text-black transition-all duration-100 hover:bg-red-700 
+            hover:text-white 2xs:text-2xl sm:top-[290px] md:text-3xl xl:top-[400px]"
+          >
+            Barmejha
+          </Link>
+          <HeaderArrow />
+        </div>
       </div>
     </header>
   );
 };
 
+const FloatingCards = async () => {
+  const activities = [
+    { src: "/images/bowling.jpg", placing: "left-[3%] top-[20%] h-56 w-40" },
+    { src: "/images/dodge.jpg", placing: "left-[33%] top-[8%] h-32 w-24" },
+    { src: "/images/billard.jpg", placing: "right-32 top-20 h-60 w-44" },
+    {
+      src: "/images/paraglider.jpg",
+      placing: "bottom-[6%] left-[12%] h-72 w-52",
+    },
+    {
+      src: "/images/basketball.jpg",
+      placing: "bottom-[14%] right-[16%] h-64 w-48",
+    },
+  ];
+  const blurData: string[] = [];
+  for (let i = 0; i < activities.length; i++) {
+    blurData.push(await dynamicBlurDataUrl(activities[i]?.src ?? ""));
+  }
+
+  return (
+    <>
+      {activities.map((activity, index) => {
+        return (
+          <Image
+            key={index}
+            tabIndex={-1}
+            src={activity.src ?? ""}
+            alt="activity"
+            placeholder="blur"
+            blurDataURL={blurData[index] ?? ""}
+            width={300}
+            height={300}
+            className={cn(
+              " absolute rounded-lg bg-slate-300 object-cover shadow-2xl brightness-50 md:brightness-75",
+              activity.placing
+            )}
+          />
+        );
+      })}
+    </>
+  );
+};
 export default Header;
