73ed2105b086a37892beece5d6b90766bfad48db
Jnayeh <mouradslim44@gmail.com>
2024-02-25 10:49:44 +0100

    adds steps to site

--- src/components/planning-steps.tsx
+++ src/components/planning-steps.tsx
@@ -3,17 +3,35 @@ import { dynamicBlurDataUrl } from "~/server/actions";
 
 export default async function PlanningSteps() {
   const blurData = await dynamicBlurDataUrl("/beach.webp");
+  const steps = [
+    {
+      src: "/steps/plan.webp",
+      alt: "plani kharjtek",
+      description: "Plan your next adventure with us",
+    },
+    {
+      src: "/steps/reserve.webp",
+      alt: "reservi li ysa3dek",
+      description: "Reserve your activity on our platform",
+    },
+    {
+      src: "/steps/live.webp",
+      alt: "3ich jawk",
+      description: "Live your adventure with yor friends",
+    },
+  ];
   return (
-    <section className="h-[85dvh] w-full overflow-hidden">
-      <div className="relative h-full w-full">
+    <section className="relative h-fit w-full">
+      <div className="relative h-fit w-full">
         <h2
-          className="absolute top-12 z-0 mx-[10%] flex w-[80%] justify-center text-center 
-            font-rubik text-[5vw] font-medium uppercase text-black sm:text-4xl md:text-5xl"
+          className=" z-10 flex w-[80%] flex-col justify-center text-center font-ubuntu text-[15vw] font-medium pt-16 sm:pt-0
+            text-white 2xs:text-7xl md:flex-row md:gap-2 sm:text-8xl md:absolute md:-top-16 mx-[10%] md:text-6xl xl:text-7xl"
         >
-          discover the best activities <br /> with one swipe
+          <span>Barmejha</span>
+          <span>in 3 words</span>
         </h2>
         <Image
-          className="h-[90dvh] w-full object-cover"
+          className="absolute -top-10 -z-10 h-[150%] w-full object-cover object-bottom"
           alt="friends sitting in front of firecamp"
           placeholder="blur"
           blurDataURL={blurData}
@@ -21,6 +39,27 @@ export default async function PlanningSteps() {
           height={1000}
           src="/beach.webp"
         />
+
+        <div className="z-10 flex h-full w-full flex-col items-center justify-center gap-6 p-4 py-32 pb-16 md:flex-row md:gap-4 md:pb-32">
+          {steps.map((step, index) => (
+            <div
+              key={index}
+              className="relative flex aspect-video w-full flex-col items-center justify-center gap-2 rounded-2xl bg-slate-100 p-3 py-4 text-center dark:bg-slate-700 md:w-1/3"
+            >
+              <h3 className=" text-[6vw] font-semibold text-[#004449] dark:text-white md:text-2xl">
+                {step.alt}
+              </h3>
+              <Image
+                src={step.src}
+                alt={step.alt}
+                className=" h-32 object-contain md:absolute md:top-0 md:-z-10 md:-translate-y-3/4"
+                width={170}
+                height={140}
+              />
+              <p className=" px-4 pt-4 md:pt-2 font-medium">{step.description}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
