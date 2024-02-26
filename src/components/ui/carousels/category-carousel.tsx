0460c8c0c5e6cab3cbd14b1c8e8b7f67bceac08f
Jnayeh <mouradslim44@gmail.com>
2024-02-26 13:46:41 +0100

    adds 'all' option to categories

--- src/components/ui/carousels/category-carousel.tsx
+++ src/components/ui/carousels/category-carousel.tsx
@@ -13,11 +13,14 @@ import {
 import { api } from "~/trpc/react";
 
 export function CategoryCarousel() {
-  const { data: categories, isLoading} = api.category.getAll.useQuery(undefined, {
-    staleTime: 1000 * 60 * 60 * 5,
-    refetchOnWindowFocus: false,
-    retry: 1,
-  });
+  const { data: categories, isLoading } = api.category.getAll.useQuery(
+    undefined,
+    {
+      staleTime: 1000 * 60 * 60 * 5,
+      refetchOnWindowFocus: false,
+      retry: 1,
+    }
+  );
   if (isLoading) return <CategoryCarouselSkeleton />;
   return (
     <Carousel
@@ -27,18 +30,30 @@ export function CategoryCarousel() {
       className="w-full max-w-fit"
     >
       <CarouselContent radioGroup="categories">
-        {categories && categories.map((cat, index) => (
-          <CarouselItem
-            key={index}
-            className=" w-fit min-w-max flex-initial pl-2 first:pl-4"
-          >
-            <Card tabIndex={0} className="bg-slate-500 text-white">
-              <CardContent className="flex items-center justify-center whitespace-nowrap py-2 px-3">
-                <span className="text-sm md:text-base font-semibold select-none">{cat.label}</span>
-              </CardContent>
-            </Card>
-          </CarouselItem>
-        ))}
+        <CarouselItem className=" w-fit min-w-max flex-initial pl-2 first:pl-4">
+          <Card tabIndex={0} className="bg-slate-500 text-white">
+            <CardContent className="flex items-center justify-center whitespace-nowrap px-3 py-2">
+              <span className="select-none text-sm font-semibold md:text-base">
+                All
+              </span>
+            </CardContent>
+          </Card>
+        </CarouselItem>
+        {categories &&
+          categories.map((cat, index) => (
+            <CarouselItem
+              key={index}
+              className=" w-fit min-w-max flex-initial pl-2 first:pl-4"
+            >
+              <Card tabIndex={0} className="bg-slate-500 text-white">
+                <CardContent className="flex items-center justify-center whitespace-nowrap px-3 py-2">
+                  <span className="select-none text-sm font-semibold md:text-base">
+                    {cat.label}
+                  </span>
+                </CardContent>
+              </Card>
+            </CarouselItem>
+          ))}
       </CarouselContent>
     </Carousel>
   );
