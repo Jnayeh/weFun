a7646b28638c83e55c42d27abaec5fd28aa77663
Jnayeh <mouradslim44@gmail.com>
2024-03-01 19:38:58 +0100

    adds not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { ThemeProvider } from "~/utils/theme-provider";
 import dynamic from "next/dynamic";
 
-const GoBack = dynamic(() => import("~/components/ui/back-button"), {
+const GoBack = dynamic(() => import("~/components/go-back-button"), {
   loading: () => (
     <Skeleton className="flex h-[36px] w-1/2 items-center justify-center gap-x-2 rounded-lg bg-slate-300 text-sm dark:bg-slate-400 sm:w-28" />
   ),
