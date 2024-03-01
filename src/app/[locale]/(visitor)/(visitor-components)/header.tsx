34bae85af295b417a8b1ba34424a0951aa9554b0
Jnayeh <mouradslim44@gmail.com>
2024-03-01 11:02:01 +0100

    changes header background svg

--- src/app/[locale]/(visitor)/(visitor-components)/header.tsx
+++ src/app/[locale]/(visitor)/(visitor-components)/header.tsx
@@ -1,28 +1,27 @@
 import Image from "next/image";
-import HeaderArrow from "../SvgStore/header-arrow";
-import { cn } from "~/utils/helpers/server";
+import HeaderArrow from "../../../../components/SvgStore/header-arrow";
+import { cn, useTranslatedElements } from "~/utils/helpers/server";
 import { dynamicBlurDataUrl } from "~/server/actions";
-import { Suspense } from "react";
-import { Button } from "../ui/button";
-import { Skeleton } from "../ui/skeleton";
+import React, { Suspense } from "react";
+import { Button } from "~/components/ui/button";
+import { Skeleton } from "~/components/ui/skeleton";
+import HeaderSpider from "~/components/SvgStore/header-spider";
+import { useTranslations } from "next-intl";
 
 const Header = () => {
+  const t = useTranslations("Home.header");
+  const headerTitle = useTranslations("Home.header.title");
+  console.log(headerTitle);
+  const key = [1, 2, 3, 4, 5];
+
   return (
     <header
       className={cn(
         "relative mx-auto flex h-[calc(110dvh-72px)] max-h-[1080px] min-h-fit flex-col items-center overflow-hidden pb-2 font-ubuntu md:h-[110dvh] md:h-[110vh] md:px-12 md:pb-20 "
       )}
     >
-      <div className="absolute h-[120vh] w-screen">
-        <Image
-          src="/new_hero.webp"
-          alt="spider web"
-          aria-hidden
-          className="bg-gradient-to-b from-[#e8614e] via-[#dd867c] to-[#e8614e] brightness-90 contrast-150"
-          fill
-          quality={100}
-          fetchPriority="high"
-        />
+      <div className="absolute h-[120vh] w-screen bg-gradient-to-b from-[#e8614e] via-[#dd867c] to-[#e8614e] brightness-90 contrast-150">
+        <HeaderSpider className="absolute h-full w-full text-current" />
       </div>
       <div className=" relative flex h-[86vh] w-full flex-col justify-center">
         <Suspense>
@@ -30,8 +29,7 @@ const Header = () => {
         </Suspense>
         <div className="z-10 flex w-full flex-col items-center justify-center pt-32 text-white shadow-black drop-shadow ">
           <h1 className=" flex flex-col justify-center text-center text-2xl font-extrabold transition-all duration-500 dark:text-white xs:text-4xl sm:text-5xl md:text-6xl ">
-            <span className="flex justify-center">Barmej Kharja</span>
-            <span className="leading-snug ">m3a Shella</span>
+            {useTranslatedElements(key, headerTitle)}
           </h1>
           <p className=" py-4 text-center text-base font-semibold capitalize transition-all duration-500 dark:text-white xs:text-xl md:text-2xl ">
             <span className="font-marhey ">و أصنع جوك مع صحابك </span> Plani
@@ -41,11 +39,11 @@ const Header = () => {
           <div className="flex w-[70%] max-w-sm justify-center transition-all duration-300 2xs:w-[90%] ">
             <HeaderArrow id="arrow-left" className="-scale-x-100" />
             <Button
-              className="mt-5 flex h-fit items-center justify-center rounded-full bg-slate-50
-            px-5 py-2 font-bold text-black transition-all hover:bg-red-700 
-            hover:text-white 2xs:text-2xl "
+              className="mt-5 flex h-fit items-center justify-center rounded-full bg-slate-50 
+              px-5 py-2 font-bold text-black transition-all hover:bg-red-700 
+              hover:text-white 2xs:text-2xl "
             >
-              Barmejha
+              {t("button")}
             </Button>
             <HeaderArrow id="arrow-right" />
           </div>
