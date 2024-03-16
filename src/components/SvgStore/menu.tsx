d1d5769687e2cf6b913cbe2bcb9f91ab50137eca
Jnayeh <mouradslim44@gmail.com>
2024-03-16 16:05:05 +0100

    adds changes to our custom SVGs

--- src/components/SvgStore/menu.tsx
+++ src/components/SvgStore/menu.tsx
@@ -2,20 +2,11 @@ import { cn } from "~/utils/helpers/server";
 import React, { ButtonHTMLAttributes, HTMLProps } from "react";
 import { useSideBarStore } from "~/store/sidebar";
 import { DotsVerticalIcon } from "@radix-ui/react-icons";
-const SvgUser: React.FC<ButtonHTMLAttributes<HTMLButtonElement>> = ({
+const MenuIcon: React.FC<ButtonHTMLAttributes<HTMLButtonElement>> = ({
   className,
-  ...props
 }) => {
-  const { toggleRightBar } = useSideBarStore();
   return (
-    <button type="button"
-      {...props}
-      className={cn(`h-fit w-fit scale-[1.8] ${className}`)}
-      onClick={toggleRightBar}
-      aria-label="login icon"
-    >
-      <DotsVerticalIcon className="w-fit"/>
-    </button>
+    <DotsVerticalIcon className={cn(`h-fit w-fit scale-[1.8] ${className}`)} />
   );
 };
-export default SvgUser;
+export default MenuIcon;
