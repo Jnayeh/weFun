3c39e83a44feee6682c5486745ac9152723cc0e3
Jnayeh <mouradslim44@gmail.com>
2024-03-18 14:36:33 +0100

    changes home page header

--- src/app/[locale]/(visitor)/(visitor-components)/header.tsx
+++ src/app/[locale]/(visitor)/(visitor-components)/header.tsx
@@ -17,20 +17,20 @@ const Header = () => {
   return (
     <header
       className={cn(
-        `relative mx-auto flex h-[calc(110dvh-72px)] max-h-[1080px] min-h-fit flex-col items-center overflow-hidden pb-2 
+        `relative mx-auto flex h-[calc(110dvh-72px)] max-h-[1300px] min-h-fit flex-col items-center overflow-hidden pb-2 
         ${defaultFont("title")} 
-        md:h-[110vh] md:px-12 md:pb-20`
+        md:h-[120vh] md:px-12 md:pb-20`
       )}
     >
       <div className="absolute h-[120vh] w-screen bg-gradient-to-b from-[#e8614e] via-[#dd867c] to-[#e8614e] brightness-90 contrast-150">
         <HeaderSpider className="absolute h-full w-full text-current" />
       </div>
-      <div className=" relative flex h-[86vh] w-full flex-col justify-center">
+      <div className=" relative flex h-screen w-full flex-col justify-center">
         <Suspense>
           <FloatingCards />
         </Suspense>
         <div className="z-10 flex w-full flex-col items-center justify-center pt-32 text-white shadow-black drop-shadow ">
-          <h1 className=" flex flex-col justify-center text-center text-2xl font-extrabold transition-all duration-500 dark:text-white xs:text-4xl sm:text-5xl md:text-6xl ">
+          <h1 className=" flex flex-col justify-center text-center text-2xl font-extrabold transition-all duration-500 dark:text-white xs:text-4xl sm:text-5xl md:text-7xl drop-shadow-sm">
             {useTranslatedElements(key, headerTitle)}
           </h1>
           <p className=" py-4 text-center text-base font-semibold capitalize transition-all duration-500 dark:text-white xs:text-xl md:text-2xl ">
@@ -66,27 +66,27 @@ const FloatingCards = async () => {
     {
       src: "/images/paraglider.webp",
       priority: false,
-      placing: "bottom-[10%] left-[22%] h-60 w-40",
+      placing: "bottom-[4%] left-[22%] h-64 w-44 brightness-75",
     },
     {
       src: "/images/hiking.webp",
       priority: true,
-      placing: " left-[10%] md:left-[3%] top-[20%] h-72 w-52",
+      placing: " left-[10%] md:left-[3%] top-[20%] h-80 w-60",
     },
     {
       src: "/images/dodge.webp",
       priority: false,
-      placing: "left-[26%] top-[10%] h-32 w-24 md:brightness-90 brightness-75",
+      placing: "left-[26%] top-[10%] h-40 w-32 ",
     },
     {
       src: "/images/billard.webp",
       priority: false,
-      placing: "right-[8%] sm:right-[10%] top-20 h-60 w-44",
+      placing: "right-[8%] sm:right-[10%] top-20 h-72 w-52",
     },
     {
       src: "/images/basketball.webp",
       priority: false,
-      placing: "bottom-4 right-[12%] sm:right-[16%] h-64 w-48",
+      placing: "bottom-0 right-[12%] sm:right-[16%] h-72 w-56",
     },
   ];
   const blurData: string[] = [];
@@ -101,7 +101,7 @@ const FloatingCards = async () => {
           <div
             key={index}
             className={cn(
-              "absolute animate-floating-card overflow-hidden rounded-2xl bg-slate-300 shadow-2xl shadow-black/60 brightness-50 md:brightness-75",
+              "absolute animate-floating-card overflow-hidden rounded-3xl bg-slate-300 shadow-xl brightness-90",
               activity.placing,
               index % 3 == 0 ? "delay-400" : index % 3 == 2 ? "" : " delay-1000"
             )}
