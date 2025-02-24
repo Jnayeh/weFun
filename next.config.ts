33300a098684e765b9c79cd4ed9ff4e3df8f8c96
jnayeh <mouradslim44@gmail.com>
2025-02-24 03:31:10 +0100

    change config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+import createNextIntlPlugin from "next-intl/plugin";
+const withNextIntl = createNextIntlPlugin();
+const nextConfig: NextConfig = withNextIntl({
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+});
 
 export default nextConfig;
